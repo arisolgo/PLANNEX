e532f7851b5a2b3b2235b69d550fb3ebdfe646eb
arisolgo <arielsolano39@gmail.com>
2021-07-23 00:40:02 -0400

    initial commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from './shared/models/shared.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, SharedModule
+  ]
+})
+export class CoreModule { }
