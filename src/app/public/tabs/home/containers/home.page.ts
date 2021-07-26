dae65d08d434091abd19821134e2c244545ab8ee
arisolgo <arielsolano39@gmail.com>
2021-07-25 21:48:17 -0400

    PLAN-14: Sharing data from parent to child component

--- src/app/public/tabs/home/containers/home.page.ts
+++ src/app/public/tabs/home/containers/home.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  serviceCategories:any = [];
 
   constructor() {}
+
+  getCategories(){
+    this.serviceCategories[0] = {
+      category: 0,
+      img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+      name: 'Peluquerías'
+    }
+    this.serviceCategories[1] = {
+      category: 1,
+      img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+      name: 'Salones'
+    }
+  }
+
+  ngOnInit() {
+    this.getCategories();
+  }
+
 
 }
