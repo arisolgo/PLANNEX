b062446d5f499f0ee37621145ce2a9b2e1ba1c78
arisolgo <arielsolano39@gmail.com>
2021-07-25 22:14:36 -0400

    PLAN-12: Carrousel component created

---
+++ src/app/public/tabs/home/components/companies-carrousel/companies-carrousel.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-companies-carrousel',
+  templateUrl: './companies-carrousel.component.html',
+  styleUrls: ['./companies-carrousel.component.scss'],
+})
+export class CompaniesCarrouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
