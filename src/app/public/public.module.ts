9570af95d92aadce8cc99927b40378fcb264dc6d
arisolgo <arielsolano39@gmail.com>
2021-07-31 02:21:15 -0400

    PLAN-23: Logo section ready in providers page

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TabsPageModule } from './tabs/tabs.module';
+import { ProvidersPageModule } from './providers/containers/providers.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule, TabsPageModule, ProvidersPageModule
   ]
 })
 export class PublicModule { }
