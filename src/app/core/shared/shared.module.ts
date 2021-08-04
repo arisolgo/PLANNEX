f29b524fd3f88d09fe95e6da4545bb018055141a
arisolgo <arielsolano39@gmail.com>
2021-08-03 21:12:08 -0400

    PLAN-9: Scheduler component created

--- src/app/core/shared/shared.module.ts
+++ src/app/core/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http'
+import { IonicModule } from '@ionic/angular';
+import { SchedulerComponent } from './components/scheduler/scheduler.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SchedulerComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule, 
+    IonicModule
   ]
 })
 export class SharedModule { }
