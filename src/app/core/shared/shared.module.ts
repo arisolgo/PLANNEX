b4e51d2592daf85b908df5be003e05492f3ca774
arisolgo <arielsolano39@gmail.com>
2021-08-08 02:49:37 -0400

    idk

--- src/app/core/shared/shared.module.ts
+++ src/app/core/shared/shared.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http'
 import { IonicModule } from '@ionic/angular';
 import { SchedulerPageModule } from './pages/scheduler/containers/scheduler.module';
+import { CategoryFilterComponent } from './components/category-filter/category-filter.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CategoryFilterComponent],
   imports: [
     CommonModule,
     HttpClientModule, 
-    IonicModule
-  ]
+    IonicModule,
+    SchedulerPageModule
+  ],
+  exports:[CategoryFilterComponent]
 })
 export class SharedModule { }
