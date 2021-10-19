513db84232fe8084a1b782bd42058cff56043204
arisolgo <arielsolano39@gmail.com>
2021-10-18 23:43:20 -0400

    feat/plan_3 getting types and provider in homePage from backend

---
+++ src/app/public/tabs/home/components/companies-carousel/providers-carousel.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-providers-carousel',
+  templateUrl: './providers-carousel.component.html',
+  styleUrls: ['./providers-carousel.component.scss'],
+})
+export class ProvidersCarouselComponent implements OnInit {
+  @Input() providers = [];
+  slideOps = {
+    initialSlide: 0,
+    slidesPerView: 1.21,
+    centeredSlides: true,
+    speed: 400,
+  };
+  constructor(public navCtrl: NavController) {}
+
+  ngOnInit() {}
+
+  goToDetailPage(business) {
+    this.navCtrl.navigateForward('/business-detail', { state: business });
+  }
+}
