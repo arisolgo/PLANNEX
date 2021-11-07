344473901810a1a37ad748abe6871086d0decf14
arisolgo <arielsolano39@gmail.com>
2021-11-06 20:29:15 -0400

    useless comments deleted

--- src/app/public/business-detail/containers/business-detail.page.ts
+++ src/app/public/business-detail/containers/business-detail.page.ts
@@ -120,40 +120,4 @@ export class BusinessDetailPage implements OnInit {
 
     await modal.present();
   }
-
-  // this.services[0] = {
-  //   name: 'Corte sencillo',
-  //   img: 'https://via.placeholder.com/80x80',
-  //   category: 'Recorte',
-  //   price: 350.00,
-  //   description:"Recorte de pelo + estilado",
-  // };
-  // this.services[1] = {
-  //   name: 'Corte completo',
-  //   img: 'https://via.placeholder.com/80x80',
-  //   category: 'Recorte',
-  //   price: 500.00,
-  //   description:"Recorte de pelo, estilado y lavado",
-  // };
-  // this.services[2] = {
-  //   name: 'Manicure',
-  //   img: 'https://via.placeholder.com/80x80',
-  //   category: 'Uñas',
-  //   price: 800.00,
-  //   description:"Corte y esmaltado de uñas de las manos.",
-  // };
-  // this.services[3] = {
-  //   name: 'Pedicure',
-  //   img: 'https://via.placeholder.com/80x80',
-  //   category: 'Uñas',
-  //   price: 800.00,
-  //   description:"Corte y esmaltado de uñas de los pies.",
-  // };
-  // this.services[4] = {
-  //   name: 'Servicio completo',
-  //   img: 'https://via.placeholder.com/80x80',
-  //   category: 'Uñas',
-  //   price: 1500.00,
-  //   description:"Corte y esmaltado de uñas de las manos y pies.",
-  // };
 }
