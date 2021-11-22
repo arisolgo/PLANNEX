3900a04695f7fc0d42ec69f735408274bf6b741a
arisolgo <arielsolano39@gmail.com>
2021-11-22 03:40:08 -0400

    google maps integrated to pick up the location coordinates

--- src/app/core/shared/shared.module.ts
+++ src/app/core/shared/shared.module.ts
@@ -7,11 +7,15 @@ import { CategoryFilterComponent } from './components/category-filter/category-f
 import { PaymentSelectionComponent } from './components/payment-selection/payment-selection.component';
 import { DominicanCurrencyPipe } from '../pipes/dominican-currency.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MapsComponent } from './components/maps/maps.component';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
 
 @NgModule({
   declarations: [
     CategoryFilterComponent,
     PaymentSelectionComponent,
+    MapsComponent,
     DominicanCurrencyPipe,
   ],
   imports: [
@@ -24,8 +28,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   exports: [
     CategoryFilterComponent,
     PaymentSelectionComponent,
+    MapsComponent,
     DominicanCurrencyPipe,
     ReactiveFormsModule,
   ],
+  providers: [Geolocation, NativeGeocoder],
 })
 export class SharedModule {}
--- src/app/public/tabs/home/containers/home.module.ts
+++ src/app/public/tabs/home/containers/home.module.ts
@@ -8,9 +8,16 @@ import { HomePageRoutingModule } from './home-routing.module';
 import { ServiceCategoriesComponent } from '../components/service-categories/service-categories.component';
 import { ProvidersCarouselComponent } from '../components/companies-carousel/providers-carousel.component';
 import { OffersCarouselComponent } from '../components/offers-carousel/offers-carousel.component';
+import { SharedModule } from 'src/app/core/shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    SharedModule,
+  ],
   declarations: [
     HomePage,
     ServiceCategoriesComponent,
